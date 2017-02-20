4735ffe2bb2c0c19ae140c93994719c99174f153
inoutw <qinai@ebay.com>
2017-02-20 15:58:26 +0800

    history Change

--- Javascript/algorithm/sortarray.js
+++ Javascript/algorithm/sortarray.js
@@ -22,6 +22,8 @@ bubbleSort([12,3,5,2,7,21,1,30]);
 
 //todo: other sort algorithm
 //插入排序:https://zh.wikipedia.org/wiki/%E6%8F%92%E5%85%A5%E6%8E%92%E5%BA%8F
+
+
 //快速排序:https://zh.wikipedia.org/wiki/%E5%BF%AB%E9%80%9F%E6%8E%92%E5%BA%8F
 //参考某个元素值，将小于它的值，放到左数组中，大于它的值的元素就放到右数组中,
 // 然后递归进行上一次左右数组的操作，返回合并的数组就是已经排好顺序的数组了
@@ -40,4 +42,5 @@ function quickSort(arr){
     return [].concat(quickSort(leftArr),[q], quickSort(rightArr));
 }
 module.exports = quickSort;
+
 //希尔排序:https://zh.wikipedia.org/wiki/%E5%B8%8C%E5%B0%94%E6%8E%92%E5%BA%8F
