e0b3ddf17bd21c8f6d72ea921bf4a73e516d2dd4
inoutw <qingmin.ai@foxmail.com>
2017-06-08 11:58:43 +0800

    [javascript] calculate age

--- jQuery/sword/jsonHandler.js
+++ jQuery/sword/jsonHandler.js
@@ -1,6 +1,15 @@
+
+
+//获取key/value
+var obj = {};
+for(var k in obj){
+    console.log(k);
+    console.log(obj[k]);
+}
+
+
 //将两个JSON对象组装到一个里面
 //targetJson 目标JSON，packJson 被组装JSON
-
 function addGroupJson(targetJson, packJson){
 
     if(targetJson && packJson){
