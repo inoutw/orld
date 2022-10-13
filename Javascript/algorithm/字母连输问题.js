c0aac05bf1545060d8b45a10b24886c21c9aba23
aiqingmin <aiqingmin@360.cn>
2022-10-13 15:02:07 +0800

    update flex

--- Javascript/algorithm/字母连输问题.js
+++ Javascript/algorithm/字母连输问题.js
@@ -8,52 +8,40 @@
 输出：false
 解释：'e' 一定需要被键入两次，但在 typed 的输出中不是这样。
 */
-const getJudgeArr = (name) => {
+const getCheckArr = (name) => {
   let nameArr = name.split("");
   let len = nameArr.length;
   let res = [];
+  let num = 1;
   for (let i = 0; i < len; i++) {
-    let num = 1;
-    let curChar = nameArr[i];
-    for (let j = i + 1; j < len; j++) {
-      if (curChar === nameArr[j]) {
-        num++;
-        i++;
-      }
+    if (nameArr[i] === nameArr[i + 1]) {
+      num++;
+      continue;
+    } else {
+      res.push({ [nameArr[i]]: num });
+      num = 1;
     }
-    let obj = { [curChar]: num };
-    res.push(obj);
   }
   return res;
 };
-function isObjectValueEqual(a, b) {
-  var aProps = Object.getOwnPropertyNames(a);
-  var bProps = Object.getOwnPropertyNames(b);
 
-  if (aProps.length != bProps.length) {
-    return false;
-  }
-
-  for (var i = 0; i < aProps.length; i++) {
-    var propName = aProps[i];
-    var propA = a[propName];
-    var propB = b[propName];
-    if (propA > propB) {
+const check = (name, typed) => {
+  let nameRes = getCheckArr(name);
+  let typedRes = getCheckArr(typed);
+  if (nameRes.length !== typedRes.length) return false;
+  for (let i = 0; i < nameRes?.length; i++) {
+    const curNameObj = nameRes[i];
+    const curTypedObj = typedRes[i];
+    if (
+      Object.keys(curNameObj)[0] === Object.keys(curTypedObj)[0] &&
+      Object.values(curNameObj)[0] <= Object.values(curTypedObj)[0]
+    ) {
+      continue;
+    } else {
       return false;
     }
   }
   return true;
-}
-const judge = (name, typed) => {
-  let nameRes = getJudgeArr(name);
-  let typedRes = getJudgeArr(typed);
-  console.log(nameRes, typedRes);
-
-  if (nameRes.legnth !== typedRes.length) return false;
-  for (let i = 0; i < nameRes?.length; i++) {
-    return isObjectValueEqual(nameRes[i], typedRes[i]);
-  }
-  return true;
 };
-judge("alex", "aaleex");
-judge("saeed", "ssaaedd");
+check("alex", "aaleex"); //true
+check("saeed", "ssaaedd"); // false
--- Javascript/closure.js
+++ Javascript/closure.js
@@ -1,62 +1,83 @@
-var lis = document.getElementsByTagName('li');
+var lis = document.getElementsByTagName("li");
 for (var i = 0; i < lis.length; i++) {
-	//立即执行函数表达式,将当前i传给函数,触发onclick事件,当前i保存在内存中。
-	lis[i].onclick = (function (i) {
-		return function () {
-			console.log(i);
-		}
-	})(i)
+  //立即执行函数表达式,将当前i传给函数,触发onclick事件,当前i保存在内存中。
+  lis[i].onclick = (function (i) {
+    return function () {
+      console.log(i);
+    };
+  })(i);
 }
 
 function fn(n) {
-	lis[n].onclick = function () {
-		alert(n)
-	}
+  lis[n].onclick = function () {
+    alert(n);
+  };
 }
 for (var i = 0; i < lis.length; i++) {
-	fn(i)
+  fn(i);
 }
 
 /**
  * a 在定义的时候就已经确定了值，而不是调用函数的时候确定
  */
-var a = 100 
-function f1() { console.log(a) }
-function f2() { var a = 200; f1() }
-f2()
-// 100
-
-var a = 100 
-function f1() { console.log(a) }
-function f2() { var a = 200; f1() }
-f2(f1)
-// 100
-
-var a = 100 
-function f2() {
-	var a = 200;
-	function f1() { 
-		console.log(a)
-	}
-	return f1
+var a = 100;
+function f1() {
+  console.log(a);
 }
-var f = f2()
-f()
-// 200
+function f2() {
+  var a = 200;
+  f1();
+}
+f2();
+// 100
 
+var a = 100;
+function f1() {
+  console.log(a);
+}
+function f2() {
+  var a = 200;
+  f1();
+}
+f2(f1);
+// 100
+
+var a = 100;
+function f2() {
+  var a = 200;
+  function f1() {
+    console.log(a);
+  }
+  return f1;
+}
+var f = f2();
+f();
+// 200
 
 let x = 1;
 function A(y) {
-    let x = 2;
-    function B(z) {
-        console.log(x+y+z);
-    }
-    return B;
+  let x = 2;
+  function B(z) {
+    console.log(x + y + z);
+  }
+  return B;
 }
 let c = A(2);
-c(3)
+c(3);
 // 7
 // y=2; z=3; x=2
 //c = function B(z) {
 // 	console.log(x+y+z)
 // }
+
+var name = "a";
+
+function outter() {
+  var name = "b";
+  function inner() {
+    console.log(name);
+    console.log(this.name);
+  }
+  inner();
+}
+outter();
